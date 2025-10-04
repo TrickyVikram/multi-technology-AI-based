88c39ee3cb114b6e0f403979e6ca5758626997dc
Gaurav Sharma <gauravsharma049049@gmail.com>
2025-10-05 00:49:43 +0530

    added controller for creating rest api for crud operation on user table

--- Spring-boot/src/main/java/com/hactoberfest/entity/User.java
+++ Spring-boot/src/main/java/com/hactoberfest/entity/User.java
@@ -1,12 +1,14 @@
 package com.hactoberfest.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
+@Table(name = "users")
 @Data
 public class User {
     @Id
