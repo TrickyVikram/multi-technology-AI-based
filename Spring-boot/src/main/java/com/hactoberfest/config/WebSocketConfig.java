6389525e0722a23d07d7de0832331b83a7e9a327
Surya Prakash Kahar <sp2002rk@gmail.com>
2025-10-05 19:40:46 +0530

    feat: Configure WebSocket with /ws endpoint and simple broker

---
+++ Spring-boot/src/main/java/com/hactoberfest/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.hactoberfest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Enable a simple message broker to carry messages back to the client on destinations prefixed with "/topic" or "/queue"
+        config.enableSimpleBroker("/topic", "/queue");
+
+        // Set prefix for messages from clients to @MessageMapping annotated methods
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Register the "/ws" endpoint, enabling SockJS fallback options so that alternate transports may be used if WebSocket is not available
+        registry.addEndpoint("/ws").withSockJS();
+    }
+}
