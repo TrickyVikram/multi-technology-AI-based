51ce776b398b0b08ad9b2668ac988fff659c658f
Surya Prakash Kahar <149003440+suryaprakash0010@users.noreply.github.com>
2025-10-05 20:17:44 +0530

    Update WebSocketConfig.java

--- Spring-boot/src/main/java/com/hactoberfest/config/WebSocketConfig.java
+++ Spring-boot/src/main/java/com/hactoberfest/config/WebSocketConfig.java
@@ -12,16 +12,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        // Enable a simple message broker to carry messages back to the client on destinations prefixed with "/topic" or "/queue"
+        // Enable simple broker for public and private messaging
         config.enableSimpleBroker("/topic", "/queue");
-
-        // Set prefix for messages from clients to @MessageMapping annotated methods
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // Register the "/ws" endpoint, enabling SockJS fallback options so that alternate transports may be used if WebSocket is not available
-        registry.addEndpoint("/ws").withSockJS();
+        // Enable CORS for frontend
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 }
